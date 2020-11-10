ebce4fe7eb72eb929256609b149891f018f52f7b
Kwon-YoungSun <68536127+Kwon-YoungSun@users.noreply.github.com>
2020-11-10 10:56:33 +0900

    Update GBOARD.sql

--- GBOARD.sql
+++ GBOARD.sql
@@ -18,7 +18,7 @@ INSERT INTO
     guestboard(gno, g_mno, body)
 VALUES(
     (SELECT NVL(MAX(gno)+1, 1001) FROM guestboard),
-    1, '�λ��� ����ϼ���!'
+    1, 'ÀÎ»ñ¸»À» µî·ÏÇÏ¼¼¿ä!'
 );
 
 SELECT
@@ -43,3 +43,31 @@ WHERE
 
 
 
+CREATE TABLE reboard(
+    bno NUMBER(5)
+        CONSTRAINT REBD_NO_PK PRIMARY KEY,
+    b_mno NUMBER(5)
+        CONSTRAINT REBD_MNO_FK REFERENCES member(mno)
+        CONSTRAINT REBD_MNO_NN NOT NULL,
+    body VARCHAR2(500 CHAR)
+        CONSTRAINT REBD_BD_NN NOT NULL,
+    upno NUMBER(5),
+    wdate DATE DEFAULT SYSDATE
+        CONSTRAINT REBD_DATE_NN NOT NULL,
+    isshow CHAR(1) DEFAULT 'Y'
+        CONSTRAINT REBD_SHOW_NN NOT NULL
+        CONSTRAINT REBD_SHOW_CK CHECK (isshow IN('Y', 'N'))
+);
+
+-- 댓글게시판에 글을 7개 등록하세요.
+INSERT INTO
+    reboard(bno, b_mno, body)
+VALUES(
+    (SELECT NVL(MAX(bno) + 1, 10001) FROM reboard), ?, ?
+);
+
+SELECT
+    NVL(MAX(bno) + 1, 10001)
+FROM
+    reboard
+;
